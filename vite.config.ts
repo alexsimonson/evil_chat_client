cb7131d43b6748cc26d05cb5cb2be6634549eaf3
Alex <alexsimonson@knights.ucf.edu>
2026-02-21 22:36:49 -0500

    utilizing certs for LAN testing on mobile devices/firefox

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +9,10 @@ export default defineConfig({
     port: 3002,
     strictPort: true, // fail instead of auto-switching
     host: true,
+    https: {
+      key: fs.readFileSync("./certs/key.pem"),
+      cert: fs.readFileSync("./certs/cert.pem"),
+    },
     proxy: {
       // Proxy API-related requests to the backend so the browser sees same-origin
       '/auth': {
